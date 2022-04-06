09fd83c6f61f35bd25be10bc04f08a864d46c995
alsornak <al.sorna01@gmail.com>
2022-04-06 11:44:33 +1000

    added server.js file

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set up middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// use routes in routes directory
+app.use(require("./routes"));
+
+// set up server
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/Social-Network-API",
+	{
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
+
+// enable debug
+mongoose.set("debug", true);
+
+// start listening on PORT
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
